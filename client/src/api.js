3e66ce413bba892630aae5ad5996930869a8273a
iltw <iltw_life@mail.ru>
2022-05-18 08:35:43 +0300

    Cryptonomicon:[1]

---
+++ client/src/api.js
@@ -0,0 +1,64 @@
+/* eslint-disable prettier/prettier */
+const API_KEY =
+  "00683ea2a0e8c114e9fe1524e915d04a4052b2d0da0a88d934beb7b47b6a402f";
+
+const tickersHandlers = new Map();
+const socket = new WebSocket(
+  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
+
+const AGGREGATE_INDEX = "5";
+
+socket.addEventListener("message", (e) => {
+  const {
+    TYPE: type,
+    FROMSYMBOL: currency,
+    PRICE: newPrice,
+  } = JSON.parse(e.data);
+  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+    return;
+  }
+
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach((fn) => fn(newPrice));
+});
+
+function sendToWebSocket(message) {
+  const stringifiedMessage = JSON.stringify(message);
+  if (socket.readyState == WebSocket.OPEN) {
+    socket.send(stringifiedMessage);
+    return;
+  }
+  socket.addEventListener(
+    "open",
+    () => {
+      socket.send(stringifiedMessage);
+    },
+    { once: true }
+  );
+}
+
+function subscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~${ticker}~USD`],
+  });
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubRemove",
+    subs: [`5~CCCAGG~${ticker}~USD`],
+  });
+}
+
+export const subscribeToTicker = (ticker, cb) => {
+  const subscribers = tickersHandlers.get(ticker) || [];
+  tickersHandlers.set(ticker, [...subscribers, cb]);
+  subscribeToTickerOnWs(ticker);
+};
+
+export const unsubscribeFromTicker = (ticker) => {
+  tickersHandlers.delete(ticker);
+  unsubscribeFromTickerOnWs(ticker);
+};
---
+++ client/src/components/AddTicker.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700"
+          >Тикер</label
+        >
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+            v-model="ticker"
+            @keydown.enter="add"
+            :disabled="disabled"
+            type="text"
+            name="wallet"
+            id="wallet"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE"
+          />
+        </div>
+      </div>
+    </div>
+    <add-button @click="add" :disabled="disabled" class="my-4" />
+  </section>
+</template>
+
+<script>
+import AddButton from "@/components/AddButton.vue";
+
+export default {
+  components: {
+    AddButton,
+  },
+
+  props: {
+    disabled: { type: Boolean, reqired: false, default: false },
+  },
+
+  emits: {
+    "add-ticker": (value) => typeof value === "string",
+  },
+
+  data() {
+    return {
+      ticker: "",
+    };
+  },
+
+  methods: {
+    add() {
+      if (this.ticker.length === 0) {
+        return;
+      }
+      this.$emit("add-ticker", this.ticker);
+      this.ticker = "";
+    },
+  },
+};
+</script>
